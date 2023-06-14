2ad0dcaa2201f663aa9c7a219351685d07e55304
Pastafredda <justspotify09@gmail.com>
2023-06-14 17:59:08 +0200

    seconda parte di footer

--- src/components/DownFooter.vue
+++ src/components/DownFooter.vue
@@ -1,16 +1,31 @@
 <script>
-
-
 export default {
     name: "DownFooter",
 }
 </script>
 <template>
-    <footer>
-        <UpFooter />
-    </footer>
+    <section>
+        <p class="copyright"> <font-awesome-icon :icon="['far', 'copyright']" /> Copyright 2012 - 2020 | Avada theme by <a
+                href="#">Theme
+                Fusion</a>| All rights reserved | powered by <a href="#">WordPress</a></p>
+    </section>
 </template>
 <style scoped lang="scss">
 @use "../styles/general.scss" as *;
 @use "../styles/variables.scss" as *;
+
+section {
+    background-color: black;
+    border-top: 1px solid $color-gray;
+}
+
+.copyright {
+    padding: 50px 0;
+    color: $color-gray;
+    text-align: center;
+
+    a {
+        color: white;
+    }
+}
 </style>
--- src/main.js
+++ src/main.js
@@ -7,9 +7,9 @@ import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 
 /* import specific icons */
-import { faAngleRight, faArrowRight, faCartShopping, faDumbbell, faMagnifyingGlass, faPlay, faUserSecret } from '@fortawesome/free-solid-svg-icons'
+import { faAngleRight, faArrowRight, faCartShopping, faCopyright, faDumbbell, faMagnifyingGlass, faPlay, faRegistered, faUserSecret } from '@fortawesome/free-solid-svg-icons'
 import { faFacebookF, faInstagram, faLinkedin, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 /* add icons to the library */
-library.add(faCartShopping, faMagnifyingGlass, faPlay, faYoutube, faArrowRight, faDumbbell, faAngleRight, faInstagram, faLinkedin, faFacebookF, faTwitter)
+library.add(faCartShopping, faMagnifyingGlass, faPlay, faYoutube, faArrowRight, faDumbbell, faAngleRight, faInstagram, faLinkedin, faFacebookF, faTwitter, faCopyright)
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')
